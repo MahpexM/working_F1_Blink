f807c693cee6a828856379f1139bc99c60481ae4
mahpexm <muhpupu@gmail.com>
2026-04-22 18:36:24 +0300

    STM32F103_PC13_LED_blink

---
+++ build/dep/main.o.mk
@@ -0,0 +1,34 @@
+build/main.o: src/main.c lib/stm32f10x.h lib/core_cm3.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h \
+ lib/system_stm32f10x.h /usr/include/newlib/stdio.h \
+ /usr/include/newlib/_ansi.h /usr/include/newlib/newlib.h \
+ /usr/include/newlib/_newlib_version.h /usr/include/newlib/sys/config.h \
+ /usr/include/newlib/machine/ieeefp.h /usr/include/newlib/sys/features.h \
+ /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stddef.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdarg.h \
+ /usr/include/newlib/sys/reent.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/sys/lock.h /usr/include/newlib/sys/stdio.h
+lib/stm32f10x.h:
+lib/core_cm3.h:
+/usr/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h:
+lib/system_stm32f10x.h:
+/usr/include/newlib/stdio.h:
+/usr/include/newlib/_ansi.h:
+/usr/include/newlib/newlib.h:
+/usr/include/newlib/_newlib_version.h:
+/usr/include/newlib/sys/config.h:
+/usr/include/newlib/machine/ieeefp.h:
+/usr/include/newlib/sys/features.h:
+/usr/include/newlib/sys/cdefs.h:
+/usr/include/newlib/machine/_default_types.h:
+/usr/lib/gcc/arm-none-eabi/13.2.1/include/stddef.h:
+/usr/lib/gcc/arm-none-eabi/13.2.1/include/stdarg.h:
+/usr/include/newlib/sys/reent.h:
+/usr/include/newlib/_ansi.h:
+/usr/include/newlib/sys/_types.h:
+/usr/include/newlib/machine/_types.h:
+/usr/include/newlib/sys/lock.h:
+/usr/include/newlib/sys/stdio.h:
---
+++ src/main.c
@@ -0,0 +1,32 @@
+// Мигание встроенным светодиодом на STM32
+
+#include <stm32f10x.h>
+#include <stdio.h>
+
+void SystemInit(void) {}
+
+void sleep(uint32_t x){
+    // Ключевое слово volatile не позволяет компилятору оптимизировать код до выкидывания пустого цикла
+    volatile uint32_t i = x;
+    while(i--)
+        ;
+}
+
+int main(void)
+{
+    // Включаем тактирование GPIOA, GPIOB и GPIOC (на всякий, но нужен только PC)
+    RCC->APB2ENR |= RCC_APB2ENR_IOPAEN | RCC_APB2ENR_IOPBEN | RCC_APB2ENR_IOPCEN;
+
+    // Настраиваем PC13 как выход (2 МГц, push‑pull)
+    GPIOC->CRH &= ~(GPIO_CRH_MODE13 | GPIO_CRH_CNF13);
+    GPIOC->CRH |= GPIO_CRH_MODE13_0;        // 2 MHz output
+    // CNF13 оставляем 0 → push‑pull
+
+    while(1)
+    {
+        GPIOC->ODR ^= GPIO_ODR_ODR13;
+        sleep(577067);
+    }
+
+//    return 0;
+}
